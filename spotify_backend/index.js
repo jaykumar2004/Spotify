87e0d4f34608aa470ac4370069a051688a902ceb
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-18 14:27:03 +0530

    passport authentication

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -3,6 +3,10 @@ const app = express();
 require("dotenv").config();
 const port = 8000;
 const mongoose = require("mongoose");
+const JwtStrategy = require("passport-jwt").Strategy,
+ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User")
 
 mongoose
   .connect(
@@ -14,6 +18,27 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to database", err);
   });
+
+//passport-jwt
+  
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "secret";
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    });
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello there this is a backend project");
